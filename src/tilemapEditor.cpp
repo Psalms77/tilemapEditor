423472a613490ebaa632174dd40d097631d4cd48
Psalms77 <pc4499520@gmail.com>
2023-10-10 18:15:29 -0700

    some basic stuff

--- src/tilemapEditor.cpp
+++ src/tilemapEditor.cpp
@@ -6,7 +6,33 @@
 #include <iostream>
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "2d Tilemap Editor");
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+    int c = 3;
+    int s = 3;
+    float size = 10.f;
 
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        mousePos = sf::Mouse::getPosition(window);
+        //std::cout << mousePos.x;
+
+
+        
+        window.clear();
+
+        window.display();
+
+    }
 
 
 
